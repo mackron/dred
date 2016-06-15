af2a895b6ca6375e220fe27be9788c76b4a66478
David Reid <mackron@gmail.com>
2016-06-16 08:23:01 +1000

    Clean up.

--- source/dred_context.h
+++ source/dred_context.h
@@ -50,13 +50,6 @@ struct dred_context
 
     // Whether or not line numbers are being shown on text editors.
     bool isShowingLineNumbers;
-
-
-    // TEMP
-    dred_text_editor* pEditor0;
-    dred_tab* pEditor0Tab;
-    dred_text_editor* pEditor1;
-    dred_tab* pEditor1Tab;
 };
 
 // dred_init
