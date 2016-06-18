d1cfd1282ed66c255d6eb386deb1c67747aa33a4
David Reid <mackron@gmail.com>
2016-06-18 10:30:07 +1000

    Make changes to version numbers less error prone.

--- source/dred_build_config.h
+++ source/dred_build_config.h
@@ -1,15 +1,25 @@
 
-#define DRED_VERSION_STRING "0.1 Preview"
+#define DRED_VERSION_MAJOR          0
+#define DRED_VERSION_MINOR          1
+#define DRED_VERSION_REVISION       0
+#define DRED_VERSION_STRING         "0.1-preview"
+#define DRED_PRODUCT_NAME           "dred"
+#define DRED_PRODUCT_DESCRIPTION    "dred"
+#define DRED_COMPANY_NAME           "David Reid"
 
 #ifdef _WIN32
 #define DRED_WIN32
+#define DRED_EXE_NAME               "dred.exe"
 #endif
 
 #ifdef __linux__
 #define DRED_GTK
+#define DRED_EXE_NAME               "dred"
 #endif
 
-#define DRED_MAX_PATH       4096
+#define DRED_MAX_PATH               4096
 
-#define DRED_MIN_FONT_SIZE  1
-#define DRED_MAX_FONT_SIZE  400
+#define DRED_MIN_FONT_SIZE          1
+#define DRED_MAX_FONT_SIZE          400
+
+// KEEP THIS BLANK LINE AT THE END. NEEDED FOR THE .RC FILE.
