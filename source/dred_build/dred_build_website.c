466183691c57a32d637172bdd14d0e30b0ae65b3
David Reid <mackron@gmail.com>
2016-07-20 21:09:48 +1000

    Start using dr_webgen for generating the website.

--- source/dred_build/dred_build_website.c
+++ source/dred_build/dred_build_website.c
@@ -1,12 +1,94 @@
 
 #include "../dred/dred_build_config.h"
 
-bool dred_build__generate_website()
+typedef struct
 {
+    command_var* pCommandVars;
+    config_var* pConfigVars;
+} dred_build_context;
+
+char* dred_build__generate_website__on_resolve_value(drwg_context* pWebgen, const char* name, const char* args, void* pUserData)
+{
+    assert(pWebgen != NULL);
+
+    //command_var* pCommandVars = (command_var*)pUserData;
+    //assert(pCommandVars != NULL);
+
+    dred_build_context* pBuildContext = (dred_build_context*)pUserData;
+    assert(pBuildContext != NULL);
+
+    char* pResultStr = NULL;
+    if (strcmp(name, "dred-command-list") == 0) {
+        int count = stb_sb_count(pBuildContext->pCommandVars);
+        for (int i = 0; i < count; ++i) {
+            drwg_config subconfig;
+            if (!drwg_config_init(&subconfig, &pWebgen->config)) {
+                return NULL;
+            }
+
+            drwg_config_set(&subconfig, "dred-command-name", pBuildContext->pCommandVars[i].name);
+
+            char* pCommandStr = drwg_context_process_file_with_config(pWebgen, &subconfig, "_drwebgen/template-command.html");
+            if (pCommandStr != NULL) {
+                pResultStr = drwg_append_string(pResultStr, pCommandStr);
+                drwg_free_string(pCommandStr);
+            }
+
+            drwg_config_uninit(&subconfig);
+        }
+
+        return pResultStr;
+    }
+
+    if (strcmp(name, "dred-config-var-list") == 0) {
+        int count = stb_sb_count(pBuildContext->pConfigVars);
+        for (int i = 0; i < count; ++i) {
+            drwg_config subconfig;
+            if (!drwg_config_init(&subconfig, &pWebgen->config)) {
+                return NULL;
+            }
+
+            drwg_config_set(&subconfig, "dred-config-var-name", pBuildContext->pConfigVars[i].name);
+            drwg_config_set(&subconfig, "dred-config-var-documentation", pBuildContext->pConfigVars[i].documentation);
+            drwg_config_set(&subconfig, "dred-config-var-type", pBuildContext->pConfigVars[i].typeSrc);
+            drwg_config_set(&subconfig, "dred-config-var-default-value", pBuildContext->pConfigVars[i].defaultValueSrc);
+
+            char* pConfigVarStr = drwg_context_process_file_with_config(pWebgen, &subconfig, "_drwebgen/template-config-var.html");
+            if (pConfigVarStr != NULL) {
+                pResultStr = drwg_append_string(pResultStr, pConfigVarStr);
+                drwg_free_string(pConfigVarStr);
+            }
+
+            drwg_config_uninit(&subconfig);
+        }
+
+        return pResultStr;
+    }
+
+    return NULL;
+}
+
+void dred_build__generate_website__on_free_value(drwg_context* pWebgen, char* valueReturnedByOnResolveValue)
+{
+    (void)pWebgen;
+    drwg_free_string(valueReturnedByOnResolveValue);
+}
+
+bool dred_build__generate_website(command_var* pCommandVars, config_var* pConfigVars)
+{
+    dred_build_context context;
+    context.pCommandVars = pCommandVars;
+    context.pConfigVars = pConfigVars;
+
     drwg_context webgen;
     if (!drwg_context_init(&webgen, "../../../source/website", "../../../build/website")) {
         return false;
     }
+
+    webgen.onResolveValue = dred_build__generate_website__on_resolve_value;
+    webgen.onFreeValue = dred_build__generate_website__on_free_value;
+    webgen.pUserData = &context;
+
 
     // Properties.
     drwg_context_set(&webgen, "dred-version", DRED_VERSION_STRING);
