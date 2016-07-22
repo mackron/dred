6ebbd36def5d50790855e913033554cb7192eee5
David Reid <mackron@gmail.com>
2016-07-22 13:11:49 +1000

    Small clean up to the GUI header.

--- source/dred/gui/dred_gui.h
+++ source/dred/gui/dred_gui.h
@@ -151,19 +151,10 @@
 // }
 //
 
-#ifndef dr_gui_h
-#define dr_gui_h
-
-#include <stdbool.h>
-
-#ifndef DRGUI_MAX_FONT_FAMILY_LENGTH
-#define DRGUI_MAX_FONT_FAMILY_LENGTH  128
+#ifndef DRED_MAX_FONT_FAMILY_LENGTH
+#define DRED_MAX_FONT_FAMILY_LENGTH  128
 #endif
 
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 typedef struct drgui_context drgui_context;
 typedef struct drgui_element drgui_element;
@@ -606,7 +597,7 @@ struct drgui_font
     drgui_context* pContext;
 
     /// The font family.
-    char family[DRGUI_MAX_FONT_FAMILY_LENGTH];
+    char family[DRED_MAX_FONT_FAMILY_LENGTH];
 
     /// The base size of the font. This is set to the value that was used to create the font in the first place.
     unsigned int size;
@@ -1508,7 +1499,7 @@ bool drgui_rect_has_volume(drgui_rect rect);
 
 /////////////////////////////////////////////////////////////////
 //
-// EASY_DRAW-SPECIFIC API
+// DR_2D-SPECIFIC API
 //
 /////////////////////////////////////////////////////////////////
 #ifndef DRGUI_NO_DR_2D
@@ -1526,38 +1517,3 @@ drgui_context* drgui_create_context_dr_2d(dr2d_context* pDrawingContext);
 void drgui_register_dr_2d_callbacks(drgui_context* pContext, dr2d_context* pDrawingContext);
 
 #endif
-
-
-#ifdef __cplusplus
-}
-#endif
-#endif  //dr_gui_h
-
-
-
-/*
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org/>
-*/
