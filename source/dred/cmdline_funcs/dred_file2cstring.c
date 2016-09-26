a3333f65e75028cf91b59e8613dba0e5b5740607
David Reid <mackron@gmail.com>
2016-09-27 06:10:54 +1000

    Add a missing source file.

---
+++ source/dred/cmdline_funcs/dred_file2cstring.c
@@ -0,0 +1,54 @@
+// Copyright (C) 2016 David Reid. See included LICENSE file.
+
+// Command: dred -f file2cstring [Input File Name] [Options]
+//    -n OutputVariableName : Specifies the name the name of the output variable. Defaults to "FileData"
+//
+// Implementation: dred_file2cstring
+
+// dred -f file2cstring
+int dred_file2cstring(int argc, char** argv)
+{
+    if (argc <= 1) {
+        return -1;  // No file specified.
+    }
+
+    const char* variableName = "FileData";
+    if (argc > 3 && strcmp(argv[2], "-n") == 0) {
+        variableName = argv[3];
+    }
+
+#ifdef _MSC_VER
+    FILE* pFile;
+    if (fopen_s(&pFile, argv[1], "rb") != 0) {
+        return -2;  // Could not find file.
+    }
+#else
+    FILE* pFile = fopen(argv[1], "rb");
+    if (pFile == NULL) {
+        return -2;  // Could not find file.
+    }
+#endif
+
+    fseek(pFile, 0, SEEK_END);
+    long fileSize = ftell(pFile);
+    if (fileSize == -1) {
+        fclose(pFile);
+        return -3;  // Failed to retrieve the size of the file.
+    }
+    fseek(pFile, 0, SEEK_SET);
+
+    unsigned char* pFileData = malloc(fileSize);
+    fread(pFileData, 1, fileSize, pFile);
+    fclose(pFile);
+
+    char* pOutputData = dred_codegen_buffer_to_c_string(pFileData, fileSize, variableName);
+    free(pFileData);
+
+    if (pOutputData == NULL) {
+        return -4;
+    }
+
+    printf("%s", pOutputData);
+    free(pOutputData);
+    return 0;
+}
