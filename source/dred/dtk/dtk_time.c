e943f310d31f402fd3f93a158be52aa71c9d4749
David Reid <mackron@gmail.com>
2019-03-17 18:04:02 +1000

    Fix compiler warnings on MSVC.

--- source/dred/dtk/dtk_time.c
+++ source/dred/dtk/dtk_time.c
@@ -29,20 +29,41 @@ size_t dtk_date_YYYYMMDD(time_t t, char* strOut, size_t strOutSize)
 #endif
 }
 
+dtk_result dtk_localtime(struct tm* lt, const time_t* t)
+{
+#ifdef _MSC_VER
+    return dtk_errno_to_result(localtime_s(lt, t));
+#else
+    if (lt == NULL) {
+        return DTK_INVALID_ARGS;
+    }
+
+    struct tm* _lt = localtime(t);
+    if (_lt != NULL) {
+        *lt = *_lt;
+    } else {
+        return DTK_ERROR;
+    }
+#endif
+}
+
 int dtk_day(time_t t)
 {
-    struct tm *lt = localtime(&t);
-    return lt->tm_mday;
+    struct tm lt;
+    dtk_localtime(&lt, &t);
+    return lt.tm_mday;
 }
 
 int dtk_month(time_t t)
 {
-    struct tm *lt = localtime(&t);
-    return lt->tm_mon + 1;
+    struct tm lt;
+    dtk_localtime(&lt, &t);
+    return lt.tm_mon + 1;
 }
 
 int dtk_year(time_t t)
 {
-    struct tm *lt = localtime(&t);
-    return lt->tm_year + 1900;
+    struct tm lt;
+    dtk_localtime(&lt, &t);
+    return lt.tm_year + 1900;
 }
